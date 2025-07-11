787a8c8b7eafe5bff17b57a122e43fa784abb59d
Yanay Zabary <yanay.tsabary@gmail.com>
2025-07-11 20:44:40 +0200

    push

--- src/services/backfill-service.ts
+++ src/services/backfill-service.ts
@@ -2,6 +2,7 @@
 import Bull from "bull";
 import { Client, ChannelType, ThreadChannel, MessageType } from "discord.js";
 import { ReplykeClient } from "@replyke/node";
+import PQueue from "p-queue";
 import { getReplykeClientForGuild } from "../events/logger";
 
 /**
@@ -109,7 +110,6 @@ export function initBackfillProcessor(discordClient: Client) {
       let done = 0;
 
       // throttle one thread per second
-      const { default: PQueue } = await import("p-queue");
       const throttle = new PQueue({ interval: 1000, intervalCap: 1 });
 
       for (const thread of allThreads) {
